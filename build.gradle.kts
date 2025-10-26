780bf7fc4336cbc65b2ae2c2793e97a8015009f2
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2025-10-26 17:09:57 -0500

    Commit ya kwanza

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java-gradle-plugin")
+    id("maven-publish")
+}
+
+group = "com.charlesmuchene.swift-android-gradle-plugin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

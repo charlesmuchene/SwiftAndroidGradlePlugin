a2780f2e6ec65daec776015d92b020dce0b9ee2d
Charles Muchene <6451102+charlesmuchene@users.noreply.github.com>
2025-10-26 21:03:52 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.gradle.plugin-publish") version "1.2.1"
+    id("com.gradle.plugin-publish") version "1.3.1"
     kotlin("jvm") version "2.2.21"
     id("java-gradle-plugin")
 }
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
